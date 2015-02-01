2e659ceb700889aa75893d59f7f62de3b8b8eb39
Dan Shannon <d.shannon.au@gmail.com>
2015-02-02 01:24:40 +1100

    Stubs for StreamServicesImplTest and UploadServicesImplTest

---
+++ Strava API v3/test/test/api/service/impl/retrofit/StreamServicesImplTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package test.api.service.impl.retrofit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author danshannon
+ *
+ */
+public class StreamServicesImplTest {
+
+	/**
+	 * Test method for {@link com.danshannon.strava.api.service.impl.retrofit.StreamServicesImpl#implementation(java.lang.String)}.
+	 */
+	@Test
+	public void testImplementation() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.danshannon.strava.api.service.impl.retrofit.StreamServicesImpl#getActivityStreams(java.lang.String, com.danshannon.strava.api.model.reference.StreamType[], com.danshannon.strava.api.model.reference.StreamResolutionType, com.danshannon.strava.api.model.reference.StreamSeriesDownsamplingType)}.
+	 */
+	@Test
+	public void testGetActivityStreams() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.danshannon.strava.api.service.impl.retrofit.StreamServicesImpl#getEffortStreams(java.lang.String, com.danshannon.strava.api.model.reference.StreamType[], com.danshannon.strava.api.model.reference.StreamResolutionType, com.danshannon.strava.api.model.reference.StreamSeriesDownsamplingType)}.
+	 */
+	@Test
+	public void testGetEffortStreams() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.danshannon.strava.api.service.impl.retrofit.StreamServicesImpl#getSegmentStreams(java.lang.String, com.danshannon.strava.api.model.reference.StreamType[], com.danshannon.strava.api.model.reference.StreamResolutionType, com.danshannon.strava.api.model.reference.StreamSeriesDownsamplingType)}.
+	 */
+	@Test
+	public void testGetSegmentStreams() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Strava API v3/test/test/api/service/impl/retrofit/UploadServicesImplTest.java
@@ -0,0 +1,24 @@
+package test.api.service.impl.retrofit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UploadServicesImplTest {
+
+	@Test
+	public void testImplementation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpload() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckUploadStatus() {
+		fail("Not yet implemented");
+	}
+
+}
