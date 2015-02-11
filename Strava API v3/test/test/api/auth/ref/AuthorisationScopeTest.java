76595a9fea6688ebedebca0b3891c0392ef1a017
Dan Shannon <dan.shannon@marlo.com.au>
2015-02-11 19:00:45 +1100

    Add missing AuthorisationScopeTest

---
+++ Strava API v3/test/test/api/auth/ref/AuthorisationScopeTest.java
@@ -0,0 +1,29 @@
+package test.api.auth.ref;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import stravajava.api.v3.auth.ref.AuthorisationScope;
+/**
+ * @author dshannon
+ *
+ */
+public class AuthorisationScopeTest {
+	@Test
+	public void testGetId() {
+		for (AuthorisationScope type : AuthorisationScope.values()) {
+			assertNotNull(type.getId());
+			assertEquals(type,AuthorisationScope.create(type.getId()));
+		}
+	}
+	
+	@Test
+	public void testGetDescription() {
+		for (AuthorisationScope type : AuthorisationScope.values()) {
+			assertNotNull(type.getDescription());
+		}
+	}
+
+}
