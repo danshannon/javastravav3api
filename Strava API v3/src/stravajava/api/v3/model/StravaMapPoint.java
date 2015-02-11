26bd0ec85dbfada5917c4d9d623740a6b31ac67b
Dan Shannon <d.shannon.au@gmail.com>
2015-02-11 23:44:09 +1100

    Futile resistance is We don't do pointless and unused methods

--- Strava API v3/src/stravajava/api/v3/model/StravaMapPoint.java
+++ Strava API v3/src/stravajava/api/v3/model/StravaMapPoint.java
@@ -16,16 +16,12 @@ public class StravaMapPoint {
 	private Float latitude;
 	private Float longitude;
 
-	public StravaMapPoint(Float latitude, Float longitude) {
+	public StravaMapPoint(final Float latitude, final Float longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
 	}
 	
-	public Float[] getValue() {
-		return new Float[]{this.latitude,this.longitude};
-	}
-	
-	public StravaMapPoint create(Float[] latlng) {
+	public StravaMapPoint create(final Float[] latlng) {
 		return new StravaMapPoint(latlng[0],latlng[1]);
 	}
 
