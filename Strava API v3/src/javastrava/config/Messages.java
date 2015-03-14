dcbde885448fb8c8d91458df1de4ba982ab10df8
Dan Shannon <d.shannon.au@gmail.com>
2015-03-14 22:18:51 +1100

    Forgotten something See https://github.com/danshannonjavastravav3api/issues/38

---
+++ Strava API v3/src/javastrava/config/Messages.java
@@ -0,0 +1,21 @@
+package javastrava.config;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Messages {
+	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+	private Messages() {
+	}
+
+	public static String getString(final String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
