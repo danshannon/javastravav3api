19474f90e9cff624c8e17d9fdd397658e9cce2ef
Dan Shannon <dan.shannon@marlo.com.au>
2015-03-18 16:53:49 +1100

    Add token received from token exchange to the TokenManager

--- Strava API v3/src/javastrava/api/v3/auth/impl/retrofit/AuthorisationServiceImpl.java
+++ Strava API v3/src/javastrava/api/v3/auth/impl/retrofit/AuthorisationServiceImpl.java
@@ -1,6 +1,7 @@
 package javastrava.api.v3.auth.impl.retrofit;
 
 import javastrava.api.v3.auth.AuthorisationService;
+import javastrava.api.v3.auth.TokenManager;
 import javastrava.api.v3.auth.model.Token;
 import javastrava.api.v3.auth.model.TokenResponse;
 import javastrava.api.v3.auth.ref.AuthorisationScope;
@@ -33,7 +34,9 @@ public class AuthorisationServiceImpl implements AuthorisationService {
 	@Override
 	public Token tokenExchange(final Integer clientId, final String clientSecret, final String code, final AuthorisationScope... scopes) throws BadRequestException, UnauthorizedException {
 		TokenResponse response = this.restService.tokenExchange(clientId, clientSecret, code);
-		return new Token(response, scopes);
+		Token token = new Token(response, scopes);
+		TokenManager.instance().storeToken(token);
+		return token;
 	}
 
 }
