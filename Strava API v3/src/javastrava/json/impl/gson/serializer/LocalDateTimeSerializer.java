2fdc45630af031678c69dd30ed5441e102820bdb
Dan Shannon <dan.shannon@marlo.com.au>
2015-03-21 13:43:43 +1100

    Custom serialisers for LocalDateTime and ZonedDateTime

---
+++ Strava API v3/src/javastrava/json/impl/gson/serializer/LocalDateTimeSerializer.java
@@ -0,0 +1,37 @@
+package javastrava.json.impl.gson.serializer;
+
+import java.lang.reflect.Type;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+/**
+ * @author dshannon
+ *
+ */
+public class LocalDateTimeSerializer implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+	/**
+	 * @see com.google.gson.JsonSerializer#serialize(java.lang.Object, java.lang.reflect.Type, com.google.gson.JsonSerializationContext)
+	 */
+	@Override
+	public JsonElement serialize(final LocalDateTime src, final Type srcType, final JsonSerializationContext context) {
+		return new JsonPrimitive(src.atZone(ZoneOffset.UTC).toString());
+	}
+
+	/**
+	 * @see com.google.gson.JsonDeserializer#deserialize(com.google.gson.JsonElement, java.lang.reflect.Type, com.google.gson.JsonDeserializationContext)
+	 */
+	@Override
+	public LocalDateTime deserialize(final JsonElement json, final Type type, final JsonDeserializationContext context) throws JsonParseException {
+		return LocalDateTime.ofInstant(Instant.parse(json.getAsString()), ZoneOffset.UTC);
+	}
+
+}
