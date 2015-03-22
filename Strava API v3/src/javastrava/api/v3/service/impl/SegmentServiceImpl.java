7df5341da63f58a4998a0a44f7d46c794f8c25ec
Dan Shannon <d.shannon.au@gmail.com>
2015-03-22 13:26:58 +1100

    Remove workaround for issue #19

--- Strava API v3/src/javastrava/api/v3/service/impl/SegmentServiceImpl.java
+++ Strava API v3/src/javastrava/api/v3/service/impl/SegmentServiceImpl.java
@@ -203,16 +203,6 @@ public class SegmentServiceImpl extends StravaServiceImpl<SegmentAPI> implements
 			}
 		});
 
-		// TODO workaround for issue javastrava-api #19 (https://github.com/danshannon/javastravav3api/issues/19)
-		if (efforts != null) {
-			for (StravaSegmentEffort effort : efforts) {
-				if (effort.getActivity().getResourceState() == null) {
-					effort.getActivity().setResourceState(StravaResourceState.META);
-				}
-			}
-		}
-		// End of workaround
-
 		// TODO workaround for issue javastrava-api #20 (https://github.com/danshannon/javastravav3api/issues/20)
 		if (efforts != null) {
 			for (StravaSegmentEffort effort : efforts) {
