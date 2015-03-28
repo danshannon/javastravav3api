112c5b567dbc1c6606a729efbc99528e46f25c45
Dan Shannon <d.shannon.au@gmail.com>
2015-03-28 17:52:59 +1100

    Fix bug when trying to deserialise null values returned in data

--- Strava API v3/src/javastrava/json/impl/gson/serializer/StravaStreamSerializer.java
+++ Strava API v3/src/javastrava/json/impl/gson/serializer/StravaStreamSerializer.java
@@ -34,40 +34,44 @@ public class StravaStreamSerializer implements JsonSerializer<StravaStream>, Jso
 		JsonObject json = null;
 		try {
 			json = (JsonObject) element;
-		} catch (ClassCastException e) { // happens if it's a primitive, or if it's crap data
+		} catch (final ClassCastException e) { // happens if it's a primitive, or if it's crap data
 			throw new JsonParseException(e);
 		}
 
 		// Get the type, as that will determine what the deserialization should do
-		StravaStreamType streamType = (StravaStreamType) context.deserialize(json.get("type"), StravaStreamType.class); //$NON-NLS-1$
+		final StravaStreamType streamType = (StravaStreamType) context.deserialize(json.get("type"), StravaStreamType.class); //$NON-NLS-1$
 
-		JsonArray array = json.getAsJsonArray("data"); //$NON-NLS-1$
+		final JsonArray array = json.getAsJsonArray("data"); //$NON-NLS-1$
 		List<StravaMapPoint> points = null;
 		List<Float> data = null;
 		List<Boolean> moving = null;
 		if (streamType == StravaStreamType.MAPPOINT) {
 			points = new ArrayList<StravaMapPoint>();
-			for (JsonElement arrayElement : array) {
-				Float latitude = Float.valueOf(arrayElement.getAsJsonArray().get(0).getAsFloat());
-				Float longitude = Float.valueOf(arrayElement.getAsJsonArray().get(1).getAsFloat());
-				StravaMapPoint point = new StravaMapPoint(latitude, longitude);
+			for (final JsonElement arrayElement : array) {
+				final Float latitude = Float.valueOf(arrayElement.getAsJsonArray().get(0).getAsFloat());
+				final Float longitude = Float.valueOf(arrayElement.getAsJsonArray().get(1).getAsFloat());
+				final StravaMapPoint point = new StravaMapPoint(latitude, longitude);
 				points.add(point);
 			}
 		} else if (streamType == StravaStreamType.MOVING) {
 			moving = new ArrayList<Boolean>();
-			for (JsonElement arrayElement : array) {
-				Boolean bool = Boolean.valueOf(arrayElement.getAsBoolean());
+			for (final JsonElement arrayElement : array) {
+				final Boolean bool = Boolean.valueOf(arrayElement.getAsBoolean());
 				moving.add(bool);
 			}
 		} else {
 			data = new ArrayList<Float>();
-			for (JsonElement arrayElement : array) {
-				Float dataElement = Float.valueOf(arrayElement.getAsFloat());
-				data.add(dataElement);
+			for (final JsonElement arrayElement : array) {
+				if (arrayElement.isJsonNull()) {
+					data.add(null);
+				} else {
+					final Float dataElement = Float.valueOf(arrayElement.getAsFloat());
+					data.add(dataElement);
+				}
 			}
 		}
 
-		StravaStream stream = new StravaStream();
+		final StravaStream stream = new StravaStream();
 		stream.setData(data);
 		stream.setMapPoints(points);
 		stream.setMoving(moving);
@@ -86,23 +90,23 @@ public class StravaStreamSerializer implements JsonSerializer<StravaStream>, Jso
 	 */
 	@Override
 	public JsonElement serialize(final StravaStream stream, final Type type, final JsonSerializationContext context) {
-		JsonObject element = new JsonObject();
+		final JsonObject element = new JsonObject();
 		element.add("original_size", context.serialize(stream.getOriginalSize())); //$NON-NLS-1$
 		element.add("resolution", context.serialize(stream.getResolution())); //$NON-NLS-1$
 		element.add("series_type", context.serialize(stream.getSeriesType())); //$NON-NLS-1$
 		element.add("type", context.serialize(stream.getType())); //$NON-NLS-1$
-		JsonArray dataArray = new JsonArray();
+		final JsonArray dataArray = new JsonArray();
 		element.add("data", dataArray); //$NON-NLS-1$
 		if (stream.getType() == StravaStreamType.MAPPOINT) {
-			for (StravaMapPoint point : stream.getMapPoints()) {
+			for (final StravaMapPoint point : stream.getMapPoints()) {
 				dataArray.add(context.serialize(point));
 			}
 		} else if (stream.getType() == StravaStreamType.MOVING) {
-			for (Boolean moving : stream.getMoving()) {
+			for (final Boolean moving : stream.getMoving()) {
 				dataArray.add(context.serialize(moving));
 			}
 		} else {
-			for (Number number : stream.getData()) {
+			for (final Number number : stream.getData()) {
 				dataArray.add(context.serialize(number));
 			}
 		}
