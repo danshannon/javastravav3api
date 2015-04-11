5a3391f06c6cb5df29bf5a73d0412caaaf9a2e82
Dan Shannon <dan.shannon@marlo.com.au>
2015-04-11 17:23:30 +1000

    Issue #100 testing failure resolved with deserialisation of garbage

--- Strava API v3/src/javastrava/json/impl/gson/serializer/StravaPhotoSourceSerializer.java
+++ Strava API v3/src/javastrava/json/impl/gson/serializer/StravaPhotoSourceSerializer.java
@@ -2,14 +2,14 @@ package javastrava.json.impl.gson.serializer;
 
 import java.lang.reflect.Type;
 
-import javastrava.api.v3.model.reference.StravaPhotoSource;
-
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
+
+import javastrava.api.v3.model.reference.StravaPhotoSource;
 
 /**
  * @author Dan Shannon
@@ -24,7 +24,12 @@ public class StravaPhotoSourceSerializer implements JsonSerializer<StravaPhotoSo
 	@Override
 	public StravaPhotoSource deserialize(final JsonElement json, final Type type, final JsonDeserializationContext context)
 			throws JsonParseException {
-		final StravaPhotoSource photoSource = StravaPhotoSource.create(Integer.valueOf(json.getAsInt()));
+		StravaPhotoSource photoSource = null;
+		try {
+			photoSource = StravaPhotoSource.create(Integer.valueOf(json.getAsInt()));
+		} catch (NumberFormatException e) {
+			throw new JsonParseException(e);
+		}
 		return photoSource;
 	}
 
